062d8ddd33ae2411dc9c46c783bf79b3f4f48add
my name <janeeshpofficial@gmail.com>
2024-03-30 14:52:12 +0530

    n

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,11 +1,13 @@
+import p from './assets/Jane.jpg'
 function Card()
 {
     return(
         <div className="card">
-            <img src="" alt="Profile"></img>
-            <h2>Janeesh</h2>
-            <p>I make you happy</p>
+            <img className="card-image" src={p} alt="Profile" style={{ width: '200px' }}></img>
+            <h2 className="card-title">Janeesh</h2>
+            <p className="card-text">I make you happy</p>
         </div>
+        
     );
 }
 export default Card
