d903f8d12b5b65edf8d84ae8b5d1109e580ef00f
my name <janeeshpofficial@gmail.com>
2024-03-30 13:18:34 +0530

    2

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card()
+{
+    return(
+        <div className="card">
+            <img src="" alt="Profile"></img>
+            <h2>Janeesh</h2>
+            <p>I make you happy</p>
+        </div>
+    );
+}
+export default Card
