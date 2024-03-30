9f4f8084d1ebfe00fcc86e822cd3cf96533d9cab
my name <janeeshpofficial@gmail.com>
2024-03-30 12:51:58 +0530

    f

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer()
+{
+    return(
+        <footer>
+            <p>
+                &copy;{new Date().getFullYear()}Janeesh is here
+            </p>
+        </footer>
+    )
+}
+export default Footer
